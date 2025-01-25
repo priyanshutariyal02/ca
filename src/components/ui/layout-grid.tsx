64460e9af408415f4b2e88e3f26c3286e7d99c6d
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2025-01-25 18:47:03 +0530

    fix error

--- src/components/ui/layout-grid.tsx
+++ src/components/ui/layout-grid.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, JSX } from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { cn } from "../../../lib/utils";
 
